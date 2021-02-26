ceec021666fde9079640f2c47daf5358db1ec550
Daniel P <dadalino@gmail.com>
2021-02-26 20:40:34 +0100

    Fix shit

--- src/media_info_screen.rs
+++ src/media_info_screen.rs
@@ -1,4 +1,3 @@
-use std::slice; // 0.2.51
 extern crate winapi;
 use crate::screen::Screen;
 use crate::screen::SpecificScreen;
@@ -8,28 +7,17 @@ use imageproc::rect::Rect;
 use regex;
 use rusttype::Font;
 use rusttype::Scale;
-use std::ffi::CStr;
-use std::ffi::CString;
 use std::ffi::OsStr;
-use std::ffi::OsString;
 use std::fmt::Debug;
-use std::io::Error;
 use std::iter::once;
-use std::os::raw::c_char;
 use std::os::windows::ffi::OsStrExt;
-use std::os::windows::prelude::*;
 use std::ptr::null_mut;
 use std::sync::{atomic::Ordering, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::{mem, ptr};
-use systemstat::{saturating_sub_bytes, Platform, System};
 use winapi::shared::minwindef::LPARAM;
-use winapi::shared::windef::{HBITMAP, HBITMAP__, HGDIOBJ, HWND, POINT, RECT, SIZE};
+use winapi::um::winuser::FindWindowW;
 use winapi::um::winuser::*;
-use winapi::um::winuser::{
-    EnumChildWindows, EnumWindows, FindWindowW, GetClientRect, GetDC, GetWindowDC, ReleaseDC,
-};
 #[derive(Debug, Clone)]
 pub struct MediaInfoScreen {
     screen: Screen,
@@ -98,9 +86,9 @@ impl MediaInfoScreen {
     }
     pub fn draw_artist(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
         let artist = self.artist.lock().unwrap();
-        let mut position_artist = (256 - (artist.len() * 16) / 2) / 2;
-        if artist.len() * 16 > 480 {
-            position_artist = 0;
+        let mut position_artist = 0;
+        if artist.len() * 16 < 480 {
+            position_artist = (256 - (artist.len() * 16) / 2) / 2;
         }
 
         draw_text_mut(
@@ -117,10 +105,7 @@ impl MediaInfoScreen {
     pub fn draw_title(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
         let title = self.title.lock().unwrap();
         let mut position_title = 0;
-
-        if title.len() * 16 > 480 {
-            position_title = 0;
-        } else {
+        if title.len() * 16 < 480 {
             position_title = (256 - (title.len() * 16) / 2) / 2;
         }
 
@@ -135,7 +120,7 @@ impl MediaInfoScreen {
         );
     }
 
-    pub fn draw_play_button(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
+    pub fn draw_play_button(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, _scale: Scale) {
         let play_button = &String::from("\u{f04B}");
         let pause_button = &String::from("\u{f04C}");
         let stop_button = &String::from("\u{f04D}");
@@ -159,7 +144,7 @@ impl MediaInfoScreen {
         );
     }
 
-    pub fn draw_elapsed(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
+    pub fn draw_elapsed(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, _scale: Scale) {
         let original = *self.track_current_position.lock().unwrap() / 1000;
         let seconds = original % 60;
         let minutes = (original / 60) % 60;
@@ -177,7 +162,7 @@ impl MediaInfoScreen {
         );
     }
 
-    pub fn draw_total(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
+    pub fn draw_total(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, _scale: Scale) {
         let original = *self.track_length.lock().unwrap();
         let seconds = original % 60;
         let minutes = (original / 60) % 60;
@@ -195,7 +180,7 @@ impl MediaInfoScreen {
         );
     }
 
-    pub fn draw_elapsed_bar(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
+    pub fn draw_elapsed_bar(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, _scale: Scale) {
         let indicator_position_x_min = 16.0;
         let indicator_position_x_max = 232.0;
 
@@ -219,7 +204,7 @@ impl MediaInfoScreen {
         let mut image = RgbImage::new(256, 64);
         let scale = Scale { x: 16.0, y: 16.0 };
 
-        if (*self.editor_active.lock().unwrap()) {
+        if *self.editor_active.lock().unwrap() {
             self.draw_artist(&mut image, scale);
             self.draw_title(&mut image, scale);
             self.draw_play_button(&mut image, scale);
@@ -277,7 +262,7 @@ impl MediaInfoScreen {
                                 // current position in msecs
                                 let mut track_current_position =
                                     SendMessageW(hwnd, WM_USER, 0, 105);
-                                if (playback_status != 1 && playback_status != 3) {
+                                if playback_status != 1 && playback_status != 3 {
                                     track_current_position = 0;
                                 }
 
