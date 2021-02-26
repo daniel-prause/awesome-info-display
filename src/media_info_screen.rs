4d018cadfc107fc435d89604c85b05d07c5a5008
Daniel P <dadalino@gmail.com>
2021-02-27 00:54:41 +0100

    Fix centering as good as possible...

--- src/media_info_screen.rs
+++ src/media_info_screen.rs
@@ -15,6 +15,7 @@ use std::ptr::null_mut;
 use std::sync::{atomic::Ordering, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
+use unicode_segmentation::UnicodeSegmentation;
 use winapi::shared::minwindef::LPARAM;
 use winapi::um::winuser::FindWindowW;
 use winapi::um::winuser::*;
@@ -28,6 +29,8 @@ pub struct MediaInfoScreen {
     title: Arc<Mutex<String>>,
     artist: Arc<Mutex<String>>,
     editor_active: Arc<Mutex<bool>>,
+    regex_first: Arc<Mutex<regex::Regex>>,
+    regex_second: Arc<Mutex<regex::Regex>>,
 }
 
 impl SpecificScreen for MediaInfoScreen {
@@ -87,8 +90,9 @@ impl MediaInfoScreen {
     pub fn draw_artist(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
         let artist = self.artist.lock().unwrap();
         let mut position_artist = 0;
-        if artist.len() * 16 < 480 {
-            position_artist = (256 - (artist.len() * 16) / 2) / 2;
+        let artist_len = artist.graphemes(true).count();
+        if artist_len * 16 < 480 {
+            position_artist = (1 + ((256 - (artist_len as u32 * 16) / 2) / 2)) - 2;
         }
 
         draw_text_mut(
@@ -104,9 +108,10 @@ impl MediaInfoScreen {
 
     pub fn draw_title(&mut self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, scale: Scale) {
         let title = self.title.lock().unwrap();
+        let title_len = title.graphemes(true).count();
         let mut position_title = 0;
-        if title.len() * 16 < 480 {
-            position_title = (256 - (title.len() * 16) / 2) / 2;
+        if title_len * 16 < 480 {
+            position_title = (1 + ((256 - (title_len as u32 * 16) / 2) / 2)) - 2;
         }
 
         draw_text_mut(
@@ -234,6 +239,8 @@ impl MediaInfoScreen {
             title: Arc::new(Mutex::new(String::new())),
             artist: Arc::new(Mutex::new(String::new())),
             editor_active: Arc::new(Mutex::new(false)),
+            regex_first: Arc::new(Mutex::new(regex::Regex::new(r"\s(.*)-").unwrap())),
+            regex_second: Arc::new(Mutex::new(regex::Regex::new(r"(.*) - (.*)").unwrap())),
         };
 
         let builder = thread::Builder::new().name("JOB_EXECUTOR".into());
@@ -255,7 +262,6 @@ impl MediaInfoScreen {
 
                         if hwnd != null_mut() {
                             unsafe {
-                                // IT WORKS!!!SendMessageW(hwnd, WM_COMMAND, 40046, 0);
                                 // 1 == playing, 3 == paused, anything else == stopped
                                 let playback_status = SendMessageW(hwnd, WM_USER, 0, 104);
                                 *this.playback_status.lock().unwrap() = playback_status;
@@ -286,9 +292,10 @@ impl MediaInfoScreen {
                                 buffer.set_len(title_length as usize);
                                 SendMessageW(hwnd, WM_GETTEXT, 3034, buffer.as_mut_ptr() as LPARAM);
                                 let data = String::from_utf16_lossy(&buffer);
-                                let re = regex::Regex::new(r"\s(.*)-").unwrap();
 
-                                if title_length == 1 || !re.is_match(&data) {
+                                if title_length == 1
+                                    || !this.regex_first.lock().unwrap().is_match(&data)
+                                {
                                     *this.editor_active.lock().unwrap() = false;
                                     thread::sleep(Duration::from_secs(1));
                                     continue;
@@ -296,17 +303,28 @@ impl MediaInfoScreen {
                                     *this.editor_active.lock().unwrap() = true;
                                 }
 
-                                let caps = re.captures(&data).unwrap();
-                                let artist_and_title = caps.get(1).map_or("", |m| m.as_str());
-                                let artist_and_title_re =
-                                    regex::Regex::new(r"(.*) - (.*)").unwrap();
-                                let artist_and_title_caps =
-                                    artist_and_title_re.captures(&artist_and_title).unwrap();
-                                let artist =
-                                    artist_and_title_caps.get(1).map_or("", |m| m.as_str());
-                                let title = artist_and_title_caps.get(2).map_or("", |m| m.as_str());
-                                *this.artist.lock().unwrap() = artist.to_string();
-                                *this.title.lock().unwrap() = title.to_string();
+                                let caps =
+                                    this.regex_first.lock().unwrap().captures(&data).unwrap();
+                                let artist_and_title =
+                                    caps.get(1).map_or("", |m| m.as_str().trim());
+
+                                let artist_and_title_caps = this
+                                    .regex_second
+                                    .lock()
+                                    .unwrap()
+                                    .captures(&artist_and_title)
+                                    .unwrap();
+                                let artist = artist_and_title_caps
+                                    .get(1)
+                                    .map_or("", |m| m.as_str().trim());
+
+                                let title = artist_and_title_caps
+                                    .get(2)
+                                    .map_or("", |m| m.as_str().trim())
+                                    .trim();
+
+                                *this.artist.lock().unwrap() = artist.trim().to_string();
+                                *this.title.lock().unwrap() = title.trim().to_string();
                             }
                         } else {
                             *this.editor_active.lock().unwrap() = false;
