42dd4ec5fbd3589cf4e7ce0a529559cd05a5393a
Daniel P <dadalino@gmail.com>
2023-02-22 00:43:51 +0100

    memory leaks must be somewhere else

--- src/screens/media_info_screen.rs
+++ src/screens/media_info_screen.rs
@@ -499,8 +499,6 @@ impl MediaInfoScreen {
 
                                                 music_player_info.artist = artist.to_string();
                                                 music_player_info.title = title.to_string();
-                                                drop(artist);
-                                                drop(title);
                                             }
                                             None => {
                                                 // check, if only artist OR title are there
@@ -514,7 +512,6 @@ impl MediaInfoScreen {
                                                             .map_or("", |m| m.as_str())
                                                             .trim();
                                                         music_player_info.title = title.to_string();
-                                                        drop(title);
                                                         music_player_info.artist = "".into();
                                                     }
                                                     None => {
