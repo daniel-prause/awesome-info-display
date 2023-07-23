e66b1ac2043da787801dde0f112b27464e690fc6
Daniel P <dadalino@gmail.com>
2023-07-23 15:43:22 +0200

    update crates and minor cleanup

--- src/helpers/convert.rs
+++ src/helpers/convert.rs
@@ -7,10 +7,3 @@ pub fn to_wstring(s: &str) -> Vec<u16> {
         .chain(std::iter::once(0))
         .collect()
 }
-
-pub fn convert_brightness(value: u16) -> f32 {
-    let old_range = 100f32 - 20f32;
-    let new_range = 100f32;
-    
-    ((value as f32 - 20f32) * new_range) / old_range
-}
