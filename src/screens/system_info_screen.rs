8f48753bfb061c0f1737655d1fd33784ba2ab7a8
Daniel P <dadalino@gmail.com>
2026-02-23 09:14:36 +0100

    Restructure system info screen

--- src/screens/system_info_screen.rs
+++ src/screens/system_info_screen.rs
@@ -1,4 +1,3 @@
-extern crate cpu_monitor;
 use crate::{
     config_manager::ConfigManager,
     screens::{BasicScreen, Screen, Screenable},
@@ -12,16 +11,16 @@ use imageproc::{
     drawing::{draw_filled_rect_mut, draw_hollow_rect_mut, draw_text_mut},
     rect::Rect,
 };
-
 use std::{
     sync::{atomic::AtomicBool, atomic::Ordering, Arc, RwLock},
     thread,
     time::Duration,
 };
 use systemstat::{saturating_sub_bytes, Platform, System};
+
 pub struct SystemInfoScreen {
     screen: Screen,
-    receiver: Receiver<SystemInfoState>,
+    receiver: Receiver<Arc<SystemInfoState>>,
 }
 
 #[derive(Default)]
@@ -38,27 +37,19 @@ impl Screenable for SystemInfoScreen {
 
 impl BasicScreen for SystemInfoScreen {
     fn update(&mut self) {
-        let system_stats = self.receiver.try_recv();
-        match system_stats {
-            Ok(system_info_state) => {
-                self.draw_screen(system_info_state.cpu_usage, system_info_state.ram_usage);
-            }
-            Err(_) => {}
+        if let Ok(system_info_arc) = self.receiver.try_recv() {
+            let system_info: &SystemInfoState = &system_info_arc;
+            self.draw_screen(system_info.cpu_usage, system_info.ram_usage);
         }
     }
 }
 
 impl SystemInfoScreen {
-    pub fn draw_cpu(
-        &mut self,
-        image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
-        cpu_usage: f64,
-        scale: PxScale,
-    ) {
-        let cpu_text = format!("{: >3}%", cpu_usage.to_string());
+    fn draw_cpu(&self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, cpu_usage: f64) {
+        let scale = PxScale { x: 16.0, y: 16.0 };
         draw_text_mut(
             image,
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
             0,
             0,
             scale,
@@ -67,36 +58,32 @@ impl SystemInfoScreen {
         );
         draw_text_mut(
             image,
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
             222,
             0,
             scale,
             &self.screen.font,
-            &cpu_text,
+            &format!("{:3.0}%", cpu_usage),
         );
+
         draw_hollow_rect_mut(
             image,
             Rect::at(0, 16).of_size(256, 10),
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
         );
-
         let cpu_filled = ((cpu_usage * 2.56) + 1.0).floor() as u32;
         draw_filled_rect_mut(
             image,
             Rect::at(0, 16).of_size(cpu_filled, 10),
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
         );
     }
-    pub fn draw_memory(
-        &mut self,
-        image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
-        ram_usage: f64,
-        scale: PxScale,
-    ) {
-        let memory_text = format!("{: >3}%", ram_usage.to_string());
+
+    fn draw_memory(&self, image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>, ram_usage: f64) {
+        let scale = PxScale { x: 16.0, y: 16.0 };
         draw_text_mut(
             image,
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
             0,
             30,
             scale,
@@ -105,34 +92,31 @@ impl SystemInfoScreen {
         );
         draw_text_mut(
             image,
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
             222,
             30,
             scale,
             &self.screen.font,
-            &memory_text,
+            &format!("{:3.0}%", ram_usage),
         );
+
         draw_hollow_rect_mut(
             image,
             Rect::at(0, 48).of_size(256, 10),
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
         );
-
         let memory_filled = ((ram_usage * 2.56) + 1.0).floor() as u32;
         draw_filled_rect_mut(
             image,
             Rect::at(0, 48).of_size(memory_filled, 10),
-            Rgb([255u8, 255u8, 255u8]),
+            Rgb([255, 255, 255]),
         );
     }
 
     fn draw_screen(&mut self, cpu_usage: f64, ram_usage: f64) {
-        // draw initial image
         let mut image = RgbImage::new(256, 64);
-        let scale = PxScale { x: 16.0, y: 16.0 };
-
-        self.draw_cpu(&mut image, cpu_usage, scale);
-        self.draw_memory(&mut image, ram_usage, scale);
+        self.draw_cpu(&mut image, cpu_usage);
+        self.draw_memory(&mut image, ram_usage);
         *self.screen.device_screen_bytes.get_mut(TEENSY).unwrap() = image.into_vec();
     }
 
@@ -142,52 +126,57 @@ impl SystemInfoScreen {
         font: FontArc,
         config_manager: Arc<RwLock<ConfigManager>>,
     ) -> SystemInfoScreen {
-        let (tx, rx): (Sender<SystemInfoState>, Receiver<SystemInfoState>) = bounded(1);
+        let (tx, rx): (Sender<Arc<SystemInfoState>>, Receiver<Arc<SystemInfoState>>) = bounded(1);
         let active = Arc::new(AtomicBool::new(false));
-        let mut this = SystemInfoScreen {
-            screen: Screen {
-                description,
-                key,
-                font,
-                active: active.clone(),
-                handle: Some(thread::spawn(move || {
+
+        let screen = Screen {
+            description,
+            key,
+            font,
+            config_manager,
+            active: active.clone(),
+            handle: Some(thread::spawn({
+                let active = active.clone();
+                move || {
                     let sys = System::new();
-                    let sender = tx.to_owned();
-                    let active = active;
+                    let sender = tx;
                     loop {
                         while !active.load(Ordering::Acquire) {
                             thread::park();
                         }
-                        let start = cpu_monitor::CpuInstant::now().unwrap();
-                        thread::sleep(Duration::from_millis(1000));
+
+                        let start = CpuInstant::now().unwrap();
+                        thread::sleep(Duration::from_secs(1));
                         let end = CpuInstant::now().unwrap();
                         let duration = end - start;
-                        let mut system_info: SystemInfoState = Default::default();
-                        system_info.cpu_usage = (duration.non_idle() * 100.0).floor();
-                        match sys.memory() {
-                            Ok(mem) => {
-                                system_info.ram_usage =
-                                    saturating_sub_bytes(mem.total, mem.free).as_u64() as f64;
-                                system_info.ram_usage =
-                                    ((system_info.ram_usage / mem.total.as_u64() as f64) * 100.0)
-                                        .floor();
 
-                                // we are right now not interested in the error value.
-                                // since we only want to have the most recent screen,
-                                // it is ok, if screen infos get lost
-                                sender.try_send(system_info).unwrap_or_default();
+                        let cpu_usage = (duration.non_idle() * 100.0).floor();
+                        let ram_usage = match sys.memory() {
+                            Ok(mem) => {
+                                let used =
+                                    saturating_sub_bytes(mem.total, mem.free).as_u64() as f64;
+                                ((used / mem.total.as_u64() as f64) * 100.0).floor()
                             }
-                            Err(x) => eprintln!("\nMemory: error: {}", x),
-                        }
+                            Err(_) => 0.0,
+                        };
+
+                        let system_info = Arc::new(SystemInfoState {
+                            cpu_usage,
+                            ram_usage,
+                        });
+                        let _ = sender.try_send(system_info);
                     }
-                })),
-                config_manager,
-                ..Default::default()
-            },
+                }
+            })),
+            ..Default::default()
+        };
+
+        let mut this = SystemInfoScreen {
+            screen,
             receiver: rx,
         };
 
-        this.draw_screen(0f64, 0f64);
+        this.draw_screen(0.0, 0.0); // initial draw
         this
     }
 }
